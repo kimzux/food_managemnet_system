f3e0bdcb21ffd3554eca35629fe339b3a5d269f5
Stanley Gama <stansgma@gmail.com>
2021-11-09 22:01:38 +0300

    update edit function validation

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,91 +6,107 @@ use App\Product;
 use App\Order_product;
 use App\Student;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 use RealRashid\SweetAlert\Facades\Alert;
 
 class ProductController extends Controller
 {
-        public function store(Request $request)
-        {
-                $check = Product::where('productName',  request('productName'))->first();
-                if (empty($check)) {
-                        $validatedData = $request->validate([
-                                'productName' => 'required|max:255',
-                                'productcategory' => 'required',
-                                'productPrice' => 'required|numeric',
-                        ]);
-                        $show = Product::create($validatedData);
-                        Alert::success('Success!', 'Successfully added');
-                        return redirect('/foodie')->withSuccessMessage('Successfully added');
-                        //   return redirect('/foodie')->with('success', 'Corona Case is successfully saved');
-                } else {
-                        Alert::warning('Warning', 'product already exist');
-                        return redirect()->back()->withWarningMessage('Product already exist');
-                }
+    public function store(Request $request)
+    {
+        $check = Product::where('productName',  request('productName'))->first();
+        if (empty($check)) {
+            $validatedData = $request->validate([
+                'productName' => 'required|max:255',
+                'productcategory' => 'required',
+                'productPrice' => 'required|numeric',
+            ]);
+            $show = Product::create($validatedData);
+            Alert::success('Success!', 'Successfully added');
+            return redirect('/foodie')->withSuccessMessage('Successfully added');
+            //   return redirect('/foodie')->with('success', 'Corona Case is successfully saved');
+        } else {
+            Alert::warning('Warning', 'product already exist');
+            return redirect()->back()->withWarningMessage('Product already exist');
         }
-        public function index()
-        {
-                $product = Product::all();
+    }
+    public function index()
+    {
+        $product = Product::all();
 
-                return view('products.index', compact('product'));
-        }
-        public function edit($id)
-        {
-                $products = Product::findOrFail($id);
+        return view('products.index', compact('product'));
+    }
 
-                return view('products.edit', compact('products'));
-        }
-        public function update(Request $request, $id)
-        {
-                $check = Product::where('productName',  request('productName'))->first();
-                if (empty($check)) {
-                        $validatedData = $request->validate([
-                                'productName' => 'required|max:255',
-                                'productcategory' => 'required',
-                                'productPrice' => 'required|numeric',
-                        ]);
-                        Product::whereId($id)->update($validatedData);
-                        Alert::success('Success!', 'Successfully updated');
-                        return redirect('/foodie')->withSuccessMessage('Successfully updated');
-                        // return redirect('/foodie')->with('success', 'Corona Case Data is successfully updated');
-                } else {
-                        Alert::warning('Warning', 'you cant update product already exist');
-                        return redirect()->back()->withWarningMessage('Product already exisst');
-                }
-        }
-        public function destroy($id)
-        {
-                $product = product::findOrFail($id);
-                $product->delete();
-                Alert::success('Success!', 'Successfully deleted');
-                return redirect('/foodie')->withSuccessMessage('Successfully deleted');
-                // return redirect('/foodie')->with('success', 'Corona Case Data is successfully deleted');
-        }
+    public function edit($id)
+    {
+        $products = Product::findOrFail($id);
 
-        public function order(Student $student)
-        {
-                $products = product::all();
-                return view('orders.choose_product', compact('products', 'student'));
-        }
+        return view('products.edit', compact('products'));
+    }
 
-        public function productstore(Request $request)
-        {
-                $order_product = new Order_product();
-                $order_product->product_id = request('product');
-                $order_product->price = request('price');
-                $order_product->date = request('date');
-                $order_product->quantity = request('quantity');
-                // $student->phone_number=request('phone');
-                // $student->registration_number=request('registration_number');
-                // $student->parent_name=request('parent_name');
-                // $student->address= request('address');
-                // $student->date_of_birth= request('dob');
-                // $student->date_of_joining= request('doj');
-                // $student->gender= request('gender');
-                // $student->branch_id=request('branch');
-                // // $student->image= request('image')->nullable
-                $order_product->save();
-                Alert::success('Success!', 'Successfully added');
-                return redirect()->back()->withSuccessMessage('success', 'Data Saved');
-        }
+    public function update(Request $request, $id)
+    {
+        // This will handle all validations.
+        $validatedData =  $request->validate([
+            'productName' => ['required', 'max:255', Rule::unique(Product::class, 'productName')->ignore($id)],
+            'productcategory' => 'required',
+            'productPrice' => 'required|numeric',
+        ]);
+
+        Product::whereId($id)->update($validatedData);
+        Alert::success('Success!', 'Successfully updated');
+        return redirect('/foodie')->withSuccessMessage('Successfully updated');
+
+        // $check = Product::where('productName',  request('productName'))->first();
+
+        // if (empty($check)) {
+        //     $validatedData = $request->validate([
+        //         'productName' => 'required|max:255',
+        //         'productcategory' => 'required',
+        //         'productPrice' => 'required|numeric',
+        //     ]);
+        //     Product::whereId($id)->update($validatedData);
+        //     Alert::success('Success!', 'Successfully updated');
+        //     return redirect('/foodie')->withSuccessMessage('Successfully updated');
+        //     // return redirect('/foodie')->with('success', 'Corona Case Data is successfully updated');
+        // } else {
+        //     Alert::warning('Warning', 'you cant update product already exist');
+        //     return redirect()->back()->withWarningMessage('Product already exisst');
+        // }
+    }
+
+    public function destroy($id)
+    {
+        $product = product::findOrFail($id);
+        $product->delete();
+        Alert::success('Success!', 'Successfully deleted');
+        return redirect('/foodie')->withSuccessMessage('Successfully deleted');
+        // return redirect('/foodie')->with('success', 'Corona Case Data is successfully deleted');
+    }
+
+    public function order(Student $student)
+    {
+        $products = product::all();
+        return view('orders.choose_product', compact('products', 'student'));
+    }
+
+    public function productstore(Request $request)
+    {
+        $order_product = new Order_product();
+        $order_product->product_id = request('product');
+        $order_product->price = request('price');
+        $order_product->date = request('date');
+        $order_product->quantity = request('quantity');
+        // $student->phone_number=request('phone');
+        // $student->registration_number=request('registration_number');
+        // $student->parent_name=request('parent_name');
+        // $student->address= request('address');
+        // $student->date_of_birth= request('dob');
+        // $student->date_of_joining= request('doj');
+        // $student->gender= request('gender');
+        // $student->branch_id=request('branch');
+        // // $student->image= request('image')->nullable
+        $order_product->save();
+        Alert::success('Success!', 'Successfully added');
+        return redirect()->back()->withSuccessMessage('success', 'Data Saved');
+    }
 }
